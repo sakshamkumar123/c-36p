a2c549d29409f68555b908ef9a5ade8e8427853d
sakshamkumar123 <71375544+sakshamkumar123@users.noreply.github.com>
2020-12-03 11:04:05 +0530

    Update Food.js

--- Food.js
+++ Food.js
@@ -21,7 +21,7 @@ class Food{
     display(){
         var x =80,y=100;
         imageMode(CENTER)
-
+        image(this.food,720,220,70,70)
 
         if (foodStock!=0){
             for(var i =0;i<foodStock;i++){
